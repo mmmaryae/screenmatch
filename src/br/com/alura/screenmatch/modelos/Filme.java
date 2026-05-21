af8d06bc757f33567e0611b31613c621dc36c3d6
maria <150175935+mmmaryae@users.noreply.github.com>
2026-05-21 17:00:41 -0300

    arrumando class filme

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -3,12 +3,11 @@ package br.com.alura.screenmatch.modelos;
 import br.com.alura.screenmatch.calculos.Classificavel;
 
 public class Filme extends Titulo implements Classificavel {
-private String diretor;
+    private String diretor;
 
-    public Filme(int anoDeLancamento, String nome) {
-        super(anoDeLancamento, nome);
+    public Filme(String nome, int anoDeLancamento) {
+        super(nome, anoDeLancamento);
     }
-
 
     public String getDiretor() {
         return diretor;
@@ -18,29 +17,13 @@ private String diretor;
         this.diretor = diretor;
     }
 
-
-
     @Override
-    /*
-    Vai ser calculado assim:
-
-Notas: 10, 8, 6
-Média: 8.0
-(int) → 8
-8 / 2 → 4
-     */
     public int getClassificacao() {
-
-        return (int) pegaMedia()/2;
+        return (int) pegaMedia() / 2;
     }
-
 
     @Override
     public String toString() {
-        return "Filme: "+ this.getNome()+" ("+this.getAnoDeLancamento()+") ";
-
-
+        return "Filme: " + this.getNome() + " (" + this.getAnoDeLancamento() + ")";
     }
-
-
 }
