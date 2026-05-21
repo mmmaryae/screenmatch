4ddb876d56288b9433d33f858e274f1d52579605
maria <150175935+mmmaryae@users.noreply.github.com>
2026-05-21 16:30:03 -0300

    arrumando record

--- src/br/com/alura/screenmatch/modelos/TituloOmdb.java
+++ src/br/com/alura/screenmatch/modelos/TituloOmdb.java
@@ -1,10 +1,4 @@
 package br.com.alura.screenmatch.modelos;
 
-
-public record TituloOmdb(String title, String year,String runtime) {
-
 public record TituloOmdb(String title, String year, String runtime) {
-    //class especial, em baixo dos panos ela já tem implementado getters and setters toString
-
-}
 }
