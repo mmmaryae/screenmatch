265bde53a3ed6a982074ba325a158c51557b27ed
maria <150175935+mmmaryae@users.noreply.github.com>
2026-05-21 16:28:55 -0300

    arrumando principal com busca

--- src/br/com/alura/screenmatch/Principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/Principal/PrincipalComBusca.java
@@ -1,11 +1,5 @@
 package br.com.alura.screenmatch.Principal;
 
-<<<<<<< HEAD
-import br.com.alura.screenmatch.modelos.Titulo;
-import com.google.gson.Gson;
-
-import javax.imageio.plugins.tiff.TIFFDirectory;
-=======
 import br.com.alura.screenmatch.excecao.ErroDeConversaoDeAnoException;
 import br.com.alura.screenmatch.modelos.Titulo;
 import br.com.alura.screenmatch.modelos.TituloOmdb;
@@ -13,7 +7,6 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
->>>>>>> 8be25bfb6d7cb0a91cddf8be30ba9acb0fae4b79
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -22,69 +15,42 @@ import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class PrincipalComBusca {
-
     public static void main(String[] args) throws IOException, InterruptedException {
-        Scanner sc =new Scanner(System.in);
+        Scanner leitura = new Scanner(System.in);
         System.out.println("Digite um filme para busca: ");
-        var busca=sc.nextLine();
-        String endereco="https://www.omdbapi.com/?t="+busca.replace(" ", "+")+"&apikey=c29ddfa1";
-        //faz a solicitação
-        try{
-        HttpClient client = HttpClient.newHttpClient();
-        //devolve a requisição --
-        HttpRequest request=HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
-        //receber a resposta do client
-        HttpResponse<String> response=client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+        var busca = leitura.nextLine();
 
-        //corpo da resposta
-<<<<<<< HEAD
-        String json=response.body();
-        System.out.println(json);
+        String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=coloque_sua_apikey";
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(endereco))
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
 
+            String json = response.body();
+            System.out.println(json);
 
-        Gson gson =new Gson();
-        Titulo meuTitulo =gson.fromJson(json, Titulo.class);
-        System.out.println(meuTitulo);
-=======
-        String json =response.body();
+            Gson gson = new GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                    .create();
 
-        System.out.println(json);
-
-        //declarando o gson-biblioteca q transforma json em objetos java
-        Gson gson=new GsonBuilder()
-
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .create();
-        //transformar em objetos -gson.fromJson
-        //Titulo meuTitulo=gson.fromJson(json, Titulo.class);
-        TituloOmdb meuTituloOmdb=gson.fromJson(json, TituloOmdb.class);
-
-        System.out.println(meuTituloOmdb);
-
-
-            Titulo meutitulo = new Titulo(meuTituloOmdb);
+            TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+            System.out.println(meuTituloOmdb);
+            //try {
+            Titulo meuTitulo = new Titulo(meuTituloOmdb);
             System.out.println("Titulo já convertido");
-            System.out.println(meutitulo);
-
-        }catch (NumberFormatException e){
+            System.out.println(meuTitulo);
+        } catch (NumberFormatException e) {
             System.out.println("Aconteceu um erro: ");
             System.out.println(e.getMessage());
         } catch (IllegalArgumentException e) {
-            System.out.println("Algum erro de argumento na busca, verifique o endereço ");
-        }
-        //exceção generica
-        catch (ErroDeConversaoDeAnoException e) {
+            System.out.println("Algum erro de argumento na busca, verifique o endereço");
+        } catch (ErroDeConversaoDeAnoException e) {
             System.out.println(e.getMessage());
-
         }
 
-
->>>>>>> 8be25bfb6d7cb0a91cddf8be30ba9acb0fae4b79
-
-
-
+        System.out.println("O programa finalizou corretamente!");
     }
 }
--- src/br/com/alura/screenmatch/modelos/TituloOmdb.java
+++ src/br/com/alura/screenmatch/modelos/TituloOmdb.java
@@ -1,9 +1,10 @@
 package br.com.alura.screenmatch.modelos;
 
-<<<<<<< HEAD
+
 public record TituloOmdb(String title, String year,String runtime) {
-=======
+
 public record TituloOmdb(String title, String year, String runtime) {
     //class especial, em baixo dos panos ela já tem implementado getters and setters toString
->>>>>>> 8be25bfb6d7cb0a91cddf8be30ba9acb0fae4b79
+
+}
 }
